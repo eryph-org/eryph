240895ffab2cdfee3484b408618b3652950f63a8
Frank Wagner <f.wagner@wagnersolutions.de>
2018-08-30 10:04:03 +0200

    move efcore dependencies to StateDb project

--- src/HyperVPlus.StateDb/InMemoryStateStoreContextConfigurer.cs
+++ src/HyperVPlus.StateDb/InMemoryStateStoreContextConfigurer.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Haipa.Runtime.Zero
 {
-    internal class InMemoryStateStoreContextConfigurer : IDbContextConfigurer<StateStoreContext> 
+    public class InMemoryStateStoreContextConfigurer : IDbContextConfigurer<StateStoreContext> 
     {
         public void Configure(DbContextOptionsBuilder options)
         {
