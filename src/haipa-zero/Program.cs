3f86e61806399d7fbd7d54bf376d402a0fdc53bb
wundersys <56606143+wundersys@users.noreply.github.com>
2019-10-29 13:31:21 +0300

    Update Program.cs

--- src/haipa-zero/Program.cs
+++ src/haipa-zero/Program.cs
@@ -18,6 +18,19 @@ namespace Haipa.Runtime.Zero
         /// <param name="args">The args<see cref="string[]"/></param>
         private static void Main(string[] args)
         {
+            var configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+              $"Haipa{Path.DirectorySeparatorChar}zero");
+
+            var privateConfigPath = Path.Combine(configPath, "private");
+            var clientsConfigPath = Path.Combine(privateConfigPath, "clients");
+
+            if (!Directory.Exists(clientsConfigPath))
+                Directory.CreateDirectory(clientsConfigPath);
+
+            File.WriteAllText(Path.Combine(clientsConfigPath, "system-client.json"),
+                "{ \"client_id\": \"system_client\", \"client_secret\" : \"388D45FA-B36B-4988-BA59-B187D329C207\" }");
+            File.WriteAllText(Path.Combine(configPath, "zero_info"),
+                $"{{ \"process_id\": \"{Process.GetCurrentProcess().Id}\", \"url\" : \"http://localhost:62189\" }}");
             var container = new Container();
             container.Bootstrap(args);
             #region Identity Server Seeder
