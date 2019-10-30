478d229fa46ad343544ba12e09c3e0d30471d240
wundersys <56606143+wundersys@users.noreply.github.com>
2019-10-30 12:07:17 +0300

    Update Program.cs

--- src/haipa-zero/Program.cs
+++ src/haipa-zero/Program.cs
@@ -18,19 +18,21 @@ namespace Haipa.Runtime.Zero
         /// <param name="args">The args<see cref="string[]"/></param>
         private static void Main(string[] args)
         {
-            var configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-              $"Haipa{Path.DirectorySeparatorChar}zero");
-
-            var privateConfigPath = Path.Combine(configPath, "private");
-            var clientsConfigPath = Path.Combine(privateConfigPath, "clients");
-
-            if (!Directory.Exists(clientsConfigPath))
-                Directory.CreateDirectory(clientsConfigPath);
-
-            File.WriteAllText(Path.Combine(clientsConfigPath, "system-client.json"),
-                "{ \"client_id\": \"system_client\", \"client_secret\" : \"388D45FA-B36B-4988-BA59-B187D329C207\" }");
-            File.WriteAllText(Path.Combine(configPath, "zero_info"),
-                $"{{ \"process_id\": \"{Process.GetCurrentProcess().Id}\", \"url\" : \"http://localhost:62189\" }}");
+        
+            Certificate.CreateSSL(new CertificateOptions
+            {
+                Issuer = Network.FQDN,
+                FriendlyName = "Haipa Zero Management Certificate",
+                Suffix = "CA",
+                ValidStartDate = DateTime.UtcNow,
+                ValidEndDate = DateTime.UtcNow.AddYears(5),
+                Password = "password",
+                ExportDirectory = Directory.GetCurrentDirectory(),
+                URL = "https://localhost:62189/",
+                AppID = "9412ee86-c21b-4eb8-bd89-f650fbf44931",
+                CACertName = "HaipaCA.pfx"
+            });
+            
             var container = new Container();
             container.Bootstrap(args);
             #region Identity Server Seeder
