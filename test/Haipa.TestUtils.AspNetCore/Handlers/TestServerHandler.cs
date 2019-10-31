5e3418d8db34aa91b5ab849dc90ec29645863e94
Frank Wagner <f.wagner@wagnersolutions.de>
2019-11-01 00:26:08 +0100

    add integration test support for webmodules

--- test/Haipa.TestUtils.AspNetCore/Handlers/TestServerHandler.cs
+++ test/Haipa.TestUtils.AspNetCore/Handlers/TestServerHandler.cs
@@ -3,7 +3,7 @@ using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Haipa.TestUtils.AspNetCore.Server
+namespace Haipa.TestUtils.Handlers
 {
     public class TestServerHandler : DelegatingHandler
     {
--- test/Haipa.TestUtils.AspNetCore/WebModuleFactoryExtensions.cs
+++ test/Haipa.TestUtils.AspNetCore/WebModuleFactoryExtensions.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using Microsoft.AspNetCore.Mvc.Testing;
+using Haipa.Modules;
+using Haipa.TestUtils.Handlers;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Simple.OData.Client;
 
-namespace Haipa.TestUtils.AspNetCore.Server
+namespace Haipa.TestUtils
 {
-    public static class WebApplicationFactoryExtensions
+    public static class WebModuleFactoryExtensions
     {
-        public static ODataClient CreateODataClient<T>(this WebApplicationFactory<T> self) where T : class
+        public static ODataClient CreateODataClient<T>(this WebModuleFactory<T> self, string uriString) where T : WebModuleBase
         {
             
             return new ODataClient(
@@ -19,21 +20,21 @@ namespace Haipa.TestUtils.AspNetCore.Server
                     {
 
                     }),
-                    new Uri("/odata", UriKind.Relative)));
+                    new Uri(uriString, UriKind.Relative)));
 
         }
 
-        public static ODataClient CreateODataClient<T>(this WebApplicationFactory<T> self, Action<IServiceCollection> services) where T : class
+        public static ODataClient CreateODataClient<T>(this WebModuleFactory<T> self, string uriString, Action<IServiceCollection> services) where T : WebModuleBase
         {
             
 
             return new ODataClient(
                 new ODataClientSettings(self.CreateAuthenticatedClient(services),
-                    new Uri("/odata", UriKind.Relative)));
+                    new Uri(uriString, UriKind.Relative)));
 
         }
 
-        public static HttpClient CreateAuthenticatedClient<T>(this WebApplicationFactory<T> self, Action<IServiceCollection> services) where T : class
+        public static HttpClient CreateAuthenticatedClient<T>(this WebModuleFactory<T> self, Action<IServiceCollection> services) where T : WebModuleBase
         {
 
             var serviceList = new Action<IServiceCollection>(services);
