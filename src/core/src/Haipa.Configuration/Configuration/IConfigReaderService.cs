c3ea0c8fd2be8739752af5af5c7d6395fd5f4913
Frank Wagner <frank2568@gmail.com>
2021-05-17 14:13:39 +0200

    restructured project files (#30)

---
+++ src/core/src/Haipa.Configuration/Configuration/IConfigReaderService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Haipa.Configuration
+{
+    public interface IConfigReaderService<out TConfig>
+    {
+        IEnumerable<TConfig> GetConfig();
+    }
+
+
+}
