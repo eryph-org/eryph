7dd3330b63963230caa02929f331444fc081047c
Frank Wagner <frank2568@gmail.com>
2021-05-17 14:12:33 +0200

    restructured project files

---
+++ src/core/src/Haipa.Configuration/Configuration/Model/ClientConfigModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Haipa.Configuration.Model
+{
+    public class ClientConfigModel
+    {
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string Description { get; set; }
+        public string X509CertificateBase64 { get; set; }
+        public string[] AllowedScopes { get; set; }
+    }
+}
