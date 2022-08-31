752359fb9e4f42e2b791371c449a4a2aa0864035
Frank Wagner <frank2568@gmail.com>
2022-08-31 18:39:56 +0200

    service mode support - added feature to run eryph-zero as windows service - serilog file logging - gitversion based file version tagging

--- src/apps/src/Eryph-zero/Program.cs
+++ src/apps/src/Eryph-zero/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Management;
 using System.Net;
@@ -19,6 +20,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+using Serilog;
 using SimpleInjector;
 using SimpleInjector.Lifestyles;
 
@@ -28,78 +33,138 @@ internal static class Program
 {
     private static async Task<int> Main(string[] args)
     {
-        await using var processLock = new ProcessFileLock(Path.Combine(ZeroConfig.GetConfigPath(), ".lock"));
+        var returnCode = 0;
 
-        var startupConfig = StartupConfiguration(args,
-            sp => new
-            {
-                BasePath = sp.GetRequiredService<IConfiguration>()["basePath"],
-                SSLEndpointManager = sp.GetRequiredService<ISSLEndpointManager>()
-            });
+        var logFilePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+            "eryph", "zero", "logs", "debug.txt");
 
-        var basePathUrl = ConfigureUrl(startupConfig.BasePath);
-         
-
-        using var _ = await startupConfig.SSLEndpointManager
-            .EnableSslEndpoint(new SSLOptions(
-            "eryph-zero CA",
-            Network.FQDN,
-            DateTime.UtcNow.AddDays(-1),
-            365 * 5,
-            ZeroConfig.GetPrivateConfigPath(),
-            "eryphCA",
-            Guid.Parse("9412ee86-c21b-4eb8-bd89-f650fbf44931"),
-            basePathUrl));
-
-        var endpoints = new Dictionary<string, string>
-        {
-            { "identity", $"{basePathUrl}identity" },
-            { "compute", $"{basePathUrl}compute" },
-            { "common", $"{basePathUrl}common" },
-        };
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File(logFilePath, 
+                rollingInterval: RollingInterval.Day, 
+                retainedFileCountLimit: 10, 
+                retainedFileTimeLimit: TimeSpan.FromDays(30))
+            .CreateLogger();
 
         try
         {
-            HostSettingsBuilder.GetHostSettings();
-        }
-        catch (ManagementException ex)
-        {
-            if (ex.ErrorCode == ManagementStatus.InvalidNamespace)
+            var fileVersion = FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location);
+            Log.Logger.Information("Starting eryph-zero {version}", fileVersion.ProductVersion);
+
+            var startupConfig = StartupConfiguration(args,
+                sp =>
+                {
+                    var configuration = sp.GetRequiredService<IConfiguration>();
+                    return new
+                    {
+                         BasePath = configuration["basePath"],
+                        SSLEndpointManager = sp.GetRequiredService<ISSLEndpointManager>(),
+                        CertificateGenerator = sp.GetRequiredService<ICertificateGenerator>(),
+
+                    };
+                });
+
+
+            await using var processLock = new ProcessFileLock(Path.Combine(ZeroConfig.GetConfigPath(), ".lock"));
+            ZeroConfig.EnsureConfiguration();
+            SystemClientGenerator.EnsureSystemClient(startupConfig.CertificateGenerator);
+
+
+            var basePathUrl = ConfigureUrl(startupConfig.BasePath);
+
+
+            using var _ = await startupConfig.SSLEndpointManager
+                .EnableSslEndpoint(new SSLOptions(
+                    "eryph-zero CA",
+                    Network.FQDN,
+                    DateTime.UtcNow.AddDays(-1),
+                    365 * 5,
+                    ZeroConfig.GetPrivateConfigPath(),
+                    "eryphCA",
+                    Guid.Parse("9412ee86-c21b-4eb8-bd89-f650fbf44931"),
+                    basePathUrl));
+
+            var endpoints = new Dictionary<string, string>
             {
-                await Console.Error.WriteAsync(
-                    "Hyper-V ist not installed. Install Hyper-V feature and then try again.");
-                return -10;
+                { "identity", $"{basePathUrl}identity" },
+                { "compute", $"{basePathUrl}compute" },
+                { "common", $"{basePathUrl}common" },
+            };
+
+            processLock.SetMetadata(new Dictionary<string, object>
+            {
+                {
+                    "endpoints", endpoints
+                }
+            });
+
+            try
+            {
+                HostSettingsBuilder.GetHostSettings();
             }
+            catch (ManagementException ex)
+            {
+                if (ex.ErrorCode == ManagementStatus.InvalidNamespace)
+                {
+                    await Console.Error.WriteAsync(
+                        "Hyper-V ist not installed. Install Hyper-V feature and then try again.");
+                    return -10;
+                }
+            }
+
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+            container.Bootstrap();
+            container.RegisterInstance<IEndpointResolver>(new EndpointResolver(endpoints));
+
+            var builder = ModulesHost.CreateDefaultBuilder(args) as ModulesHostBuilder;
+
+            var host =
+
+                    builder
+                    .ConfigureInternalHost(hb =>
+                    {
+                        hb.UseWindowsService(cfg=>cfg.ServiceName = "eryph-zero");
+                    })
+                    .UseAspNetCore((module, webHostBuilder) =>
+                    {
+                        webHostBuilder.UseHttpSys(options => { options.UrlPrefixes.Add(module.Path); });
+                    })
+                    .UseSimpleInjector(container)
+                    .HostModule<CommonApiModule>()
+                    .HostModule<ComputeApiModule>()
+                    .AddIdentityModule(container)
+                    .HostModule<VmHostAgentModule>()
+                    .AddControllerModule(container)
+                    .ConfigureServices(c => c.AddSingleton(_ => container.GetInstance<IEndpointResolver>()))
+                    .ConfigureServices(LoggerProviderOptions.RegisterProviderOptions<
+                        EventLogSettings, EventLogLoggerProvider>)
+                    .ConfigureLogging((context, logging) =>
+                    {
+                        logging.AddSerilog();
+                        // See: https://github.com/dotnet/runtime/issues/47303
+                        logging.AddConfiguration(
+                            context.Configuration.GetSection("Logging"));
+                    })
+                    .Build();
+
+            //starting here all errors should be considered as recoverable
+            returnCode = -1;
+
+
+            await host.RunAsync();
+
+            return returnCode;
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Fatal(ex, "eryph-zero failure");
+
+            return returnCode;
         }
 
-        var container = new Container();
-        container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-        container.Bootstrap();
-        container.RegisterInstance<IEndpointResolver>(new EndpointResolver(endpoints));
-
-        var host = ModulesHost.CreateDefaultBuilder(args)
-            .UseAspNetCore((module, webHostBuilder) =>
-            {
-                webHostBuilder.UseHttpSys(options => { options.UrlPrefixes.Add(module.Path); });
-            })
-            .UseSimpleInjector(container)
-            .HostModule<CommonApiModule>()
-            .HostModule<ComputeApiModule>()
-            .AddIdentityModule(container)
-            .HostModule<VmHostAgentModule>()
-            .AddControllerModule(container)
-            .ConfigureServices(c => c.AddSingleton(_ => container.GetInstance<IEndpointResolver>()))
-            .Build();
-
-        processLock.SetMetadata(new Dictionary<string, object>
-        {
-            {
-                "endpoints", endpoints
-            }
-        });
-
-        await host.RunAsync();
-        return 0;
+        
     }
 
     private static Uri ConfigureUrl(string basePath)
@@ -120,7 +185,7 @@ internal static class Program
 
                 config.AddInMemoryCollection(new Dictionary<string, string>
                     {
-                        { "basePath", "https://localhost:0" },
+                        { "basePath", "https://localhost:0" }
                     })
                     // ReSharper disable once StringLiteralTypo
                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
@@ -145,15 +210,8 @@ internal static class Program
             })
             .Build();
 
-        var certificateGenerator = configHost.Services.GetRequiredService<ICertificateGenerator>();
         var res = selectResult(configHost.Services);
-
- 
-        ZeroConfig.EnsureConfiguration();
-        SystemClientGenerator.EnsureSystemClient(certificateGenerator);
         
-        configHost.Dispose();
-
         return res;
     }
     
@@ -164,4 +222,6 @@ internal static class Program
         socket.Bind(DefaultLoopbackEndpoint);
         return (((IPEndPoint)socket.LocalEndPoint)!).Port;
     }
+
+
 }
