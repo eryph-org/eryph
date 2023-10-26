ceaad63b77ed73e7bed670bc7434873e36164be1
Frank Wagner <frank2568@gmail.com>
2023-10-26 07:28:52 +0200

    remove ovs port when catlet is destroyed (#104)
    closes #97

---
+++ src/modules/src/Eryph.Modules.VmHostAgent/Networks/OVS/IOVSPortManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eryph.VmManagement;
+using Eryph.VmManagement.Data.Full;
+using LanguageExt;
+using LanguageExt.Common;
+
+namespace Eryph.Modules.VmHostAgent.Networks.OVS;
+
+internal interface IOVSPortManager
+{
+    EitherAsync<Error, Unit> SyncPorts(TypedPsObject<VirtualMachineInfo> vmInfo, VMPortChange change);
+    EitherAsync<Error, Unit> SyncPorts(Guid vmId, VMPortChange change);
+
+
+}
