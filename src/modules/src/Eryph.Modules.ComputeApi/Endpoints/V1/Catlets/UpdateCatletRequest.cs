ab4fcfde54e47033d802c63814d3c977c975c531
Frank Wagner <frank2568@gmail.com>
2023-10-26 12:37:04 +0200

    correctly generate put request body api spec (#105)

--- src/modules/src/Eryph.Modules.ComputeApi/Endpoints/V1/Catlets/UpdateCatletRequest.cs
+++ src/modules/src/Eryph.Modules.ComputeApi/Endpoints/V1/Catlets/UpdateCatletRequest.cs
@@ -2,19 +2,23 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using Eryph.Modules.AspNetCore.ApiProvider.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Eryph.Modules.ComputeApi.Endpoints.V1.Catlets;
 
 public class UpdateCatletRequest : SingleEntityRequest
 {
-    [Required] public Guid CorrelationId { get; set; }
+    [Required]
+    [FromBody]
+    public UpdateCatletRequestBody Body { get; set; }
 
-    [Required] public JsonElement? Configuration { get; set; }
+}
 
-    [Required] public new string Id
-    {
-        get => base.Id;
-        set => base.Id = value;
-    }
+public class UpdateCatletRequestBody
+{
+    [Required]
+    public Guid CorrelationId { get; set; }
 
+    [Required]
+    public JsonElement? Configuration { get; set; }
 }
