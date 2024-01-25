1c5d0d6c9da9f42270f0ce030c27e22b77ad0caa
Christopher Mann <christopher.mann@dbosoft.eu>
2024-01-25 15:07:26 +0100

    Add self-delete for uninstaller

--- src/apps/src/eryph-uninstaller/App.xaml.cs
+++ src/apps/src/eryph-uninstaller/App.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,54 @@ namespace Eryph.Runtime.Uninstaller
     /// </summary>
     public partial class App : Application
     {
+        private bool _deleteOnExit = false;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+                return;
+#endif
+            if (e.Args.Length > 0 && e.Args[0] == "-continue")
+            {
+                _deleteOnExit = true;
+                return;
+            }
+
+            var currentProcess = Process.GetCurrentProcess();
+            var currentExecutablePath = currentProcess!.MainModule!.FileName;
+            var tempFolderPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            var tempExecutablePath = Path.Combine(tempFolderPath, Path.GetFileName(currentExecutablePath));
+            
+            Directory.CreateDirectory(tempFolderPath);
+            File.Copy(currentExecutablePath, tempExecutablePath, true);
+            
+            var startInfo = new ProcessStartInfo(tempExecutablePath)
+            {
+                Arguments = "-continue",
+                UseShellExecute = true,
+            };
+            Process.Start(startInfo);
+            
+            Shutdown();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            if (!_deleteOnExit)
+                return;
+            
+            var currentProcess = Process.GetCurrentProcess();
+            var executablePath = currentProcess!.MainModule!.FileName;
+            var folderPath = Path.GetDirectoryName(executablePath);
+
+            var startInfo = new ProcessStartInfo("powershell.exe", "")
+            {
+                Arguments = @$"-C ""Wait-Process -Id {currentProcess.Id}; Remove-Item -LiteralPath '{executablePath}';Remove-Item -LiteralPath '{folderPath}';""",
+                UseShellExecute = true,
+                CreateNoWindow = true,
+            };
+            Process.Start(startInfo);
+        }
     }
 }
