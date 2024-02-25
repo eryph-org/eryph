341f2ccd04a4e87f86cbbcae6ea50d4979a5edaf
Frank Wagner <frank2568@gmail.com>
2024-02-25 19:00:02 +0100

    wip

--- src/modules/test/Eryph.Modules.Controller.Tests/Networks/CatletIpManagerTests.cs
+++ src/modules/test/Eryph.Modules.Controller.Tests/Networks/CatletIpManagerTests.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Eryph.ConfigModel.Catlets;
-using Eryph.ConfigModel.Networks;
+﻿using Eryph.ConfigModel.Catlets;
 using Eryph.Modules.Controller.Networks;
 using Eryph.StateDb;
 using Eryph.StateDb.Model;
+using Eryph.StateDb.Specifications;
 using FluentAssertions;
 using FluentAssertions.LanguageExt;
 using Moq;
@@ -21,18 +16,29 @@ namespace Eryph.Modules.Controller.Tests.Networks
         [Fact]
         public async Task Adds_catlet_network_port()
         {
+            var networkConfig = new CatletNetworkConfig
+            {
+            };
+
+            var catletPort = new CatletNetworkPort
+            {
+                Id = Guid.NewGuid()
+            };
+
+
             var poolManager = new Mock<IIpPoolManager>();
 
             var assignmentRepo = new Mock<IStateStoreRepository<IpAssignment>>();
-            assignmentRepo.Name.ReturnsAsync(new[] { new IpAssignment() });
+            assignmentRepo.Setup(x=>x.ListAsync(
+                    new IPAssignmentSpecs.GetByPort(catletPort.Id),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new[] { new IpAssignment() }.ToList);
 
             var stateStore = new Mock<IStateStore>();
             stateStore.Setup(x => x.For<IpAssignment>()).Returns(assignmentRepo.Object);
 
             var ipManager = new CatletIpManager(stateStore.Object, poolManager.Object);
 
-            var networkConfig = new CatletNetworkConfig();
-            var catletPort = new CatletNetworkPort();
 
             var res = await ipManager.ConfigurePortIps(new Guid(), "default", catletPort, new[] { networkConfig }, CancellationToken.None);
             var addresses = res.Should().BeRight().Which;
