d22e1d9aaeb650463cde71cfa90e8823dbb4a7e9
Frank Wagner <frank2568@gmail.com>
2024-02-27 20:00:14 +0100

    expand project in VirtualNetwork model

--- src/modules/src/Eryph.Modules.ComputeApi/Model/VirtualNetworkSpecBuilder.cs
+++ src/modules/src/Eryph.Modules.ComputeApi/Model/VirtualNetworkSpecBuilder.cs
@@ -21,7 +21,8 @@ public class VirtualNetworkSpecBuilder : ResourceSpecBuilder<StateDb.Model.Virtu
     {
         var sufficientRoles = _userRightsProvider.GetResourceRoles<StateDb.Model.VirtualNetwork>(AccessRight.Read);
         return new ResourceSpecs<StateDb.Model.VirtualNetwork>
-            .GetAll(_userRightsProvider.GetAuthContext(), sufficientRoles, request.Project.GetValueOrDefault());
+            .GetAll(_userRightsProvider.GetAuthContext(), sufficientRoles, request.Project.GetValueOrDefault(), 
+                query => query.Include(x=>x.Project));
     }
 
 }
