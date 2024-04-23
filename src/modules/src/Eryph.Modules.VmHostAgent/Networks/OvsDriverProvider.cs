e4716843262e2e7b0d053a609a20eda5c2f0b286
Christopher Mann <christopher.mann@dbosoft.eu>
2024-04-23 12:05:59 +0200

    Fix scheduling

--- src/modules/src/Eryph.Modules.VmHostAgent/Networks/OvsDriverProvider.cs
+++ src/modules/src/Eryph.Modules.VmHostAgent/Networks/OvsDriverProvider.cs
@@ -186,7 +186,9 @@ public class OvsDriverProvider<RT> where RT : struct,
 
     internal static Aff<RT, Unit> waitUntilDriverServiceHasStopped() =>
         from _ in repeatWhile(
-            Schedule.spaced(TimeSpan.FromSeconds(5)) & Schedule.upto(TimeSpan.FromMinutes(5)),
+            Schedule.NoDelayOnFirst
+            & Schedule.spaced(TimeSpan.FromSeconds(5))
+            & Schedule.upto(TimeSpan.FromMinutes(5)),
             from _ in logInformation("Checking if driver service has stopped...")
             from isRunning in isDriverServiceRunning()
             select isRunning,
@@ -196,7 +198,9 @@ public class OvsDriverProvider<RT> where RT : struct,
     internal static Aff<RT, Unit> waitUntilSwitchExtensionIsEnabled(Guid switchId) =>
         from hostNetworkCommands in default(RT).HostNetworkCommands
         from _ in repeatUntil(
-            Schedule.spaced(TimeSpan.FromSeconds(5)) & Schedule.upto(TimeSpan.FromMinutes(5)),
+            Schedule.NoDelayOnFirst
+            & Schedule.spaced(TimeSpan.FromSeconds(5))
+            & Schedule.upto(TimeSpan.FromMinutes(5)),
             from _ in logInformation("Checking if OVS Hyper-V switch extension is enabled...")
             from extensionInfos in hostNetworkCommands.GetSwitchExtensions()
             select extensionInfos.Find(e => e.SwitchId == switchId)
