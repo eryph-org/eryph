7d4160f88e0d6006092f9023ab06d22c5bb10974
Christopher Mann <christopher.mann@dbosoft.eu>
2024-04-26 16:32:04 +0200

    Add test for pending model changes

---
+++ src/data/test/Eryph.StateDb.Tests/PendingMigrationsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eryph.StateDb.Design.Factories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Eryph.StateDb.Tests;
+
+public class PendingMigrationsTests
+{
+    [Theory]
+    [InlineData(typeof(MySqlStateStoreContextDesignTimeFactory))]
+    [InlineData(typeof(SqliteStateStoreContextDesignTimeFactory))]
+    [InlineData(typeof(SqlServerStateStoreContextDesignTimeFactory))]
+    public async Task StateStore_has_no_pending_model_changes(
+        Type designTimeFactory)
+    {
+        var factory = (IDesignTimeDbContextFactory<StateStoreContext>)
+            Activator.CreateInstance(designTimeFactory)!;
+        
+        await using var dbContext = factory.CreateDbContext(Array.Empty<string>());
+
+        dbContext.Database.HasPendingModelChanges().Should().BeFalse();
+    }
+}
