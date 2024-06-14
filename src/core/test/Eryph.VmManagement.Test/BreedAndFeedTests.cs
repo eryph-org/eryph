d925e91ae2d214d9a0b8b68159377a7692f5968c
Christopher Mann <christopher.mann@dbosoft.eu>
2024-06-14 18:03:16 +0200

    * Normalize variables when normalizing the catlet config
    * Add additional tests

--- src/core/test/Eryph.VmManagement.Test/BreedAndFeedTests.cs
+++ src/core/test/Eryph.VmManagement.Test/BreedAndFeedTests.cs
@@ -1,5 +1,6 @@
 ﻿using Eryph.ConfigModel;
 using Eryph.ConfigModel.Catlets;
+using Eryph.ConfigModel.Variables;
 using Eryph.GenePool.Model;
 using FluentAssertions;
 using FluentAssertions.LanguageExt;
@@ -52,6 +53,85 @@ namespace Eryph.VmManagement.Test
             newConfig.Should().NotBeNull();
             newConfig.Drives.Should().HaveCount(1);
             newConfig.Drives?.Should().ContainSingle(d => d.Source == "gene:dbosoft/utt/1.0:sda");
+        }
+
+        [Theory]
+        [InlineData(null, "false")]
+        [InlineData(false, "false")]
+        [InlineData(true, "false")]
+        [InlineData(null, "true")]
+        [InlineData(false, "true")]
+        [InlineData(true, "true")]
+        public void Child_uses_fodder_variable_binding(bool? catletSecret, string geneSecret)
+        {
+            var config = new CatletConfig
+            {
+                Name = "test",
+                Parent = "parent",
+                Fodder =
+                [
+                    new FodderConfig()
+                    {
+                        Source = "gene:dbosoft/utt/1.0:gene1",
+                        Variables =
+                        [
+                            new VariableConfig()
+                            {
+                                Name = "foodVariable",
+                                Value = "catlet value",
+                                Secret = catletSecret,
+
+                            },
+                        ],
+                    },
+                ],
+            };
+
+            var genepoolReader = new Mock<ILocalGenepoolReader>();
+
+            genepoolReader.Setup(x => x.GetGenesetReference(It.IsAny<GeneSetIdentifier>()))
+                .Returns((GeneSetIdentifier id) => Option<GeneSetIdentifier>.None);
+            genepoolReader.Setup(x => x.ReadGeneContent(GeneType.Fodder,
+                    new GeneIdentifier(GeneSetIdentifier.New("dbosoft/utt/1.0"), GeneName.New("gene1"))))
+                .Returns(
+                    $$"""
+                    {
+                      "name": "gene1",
+                      "variables": [
+                        {
+                          "name": "foodVariable",
+                          "value": "gene value",
+                          "type": "String",
+                          "required": false,
+                          "secret": "{{geneSecret}}"
+                        }
+                      ],
+                      "fodder": [
+                        {
+                          "name": "food1",
+                          "content": "test1"
+                        }
+                      ]
+                    }
+                    """
+                );
+
+            var result = config.BreedAndFeed(genepoolReader.Object, Option<CatletConfig>.None);
+
+            var newConfig = result.Should().BeRight().Subject;
+
+            newConfig.Fodder.Should().SatisfyRespectively(
+                fodder =>
+                {
+                    fodder.Name.Should().Be("food1");
+                    fodder.Variables.Should().SatisfyRespectively(
+                        variable =>
+                        {
+                            variable.Name.Should().Be("foodVariable");
+                            variable.Value.Should().Be("catlet value");
+                            variable.Secret.Should().Be(catletSecret | bool.Parse(geneSecret));
+                        });
+                });
         }
 
         [Theory]
