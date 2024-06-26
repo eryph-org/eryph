aaf0886fb539f7286c4225a850b52989c1d75db4
Christopher Mann <christopher.mann@dbosoft.eu>
2024-06-26 12:00:34 +0200

    * Skip reserved catlet fodder genes when preparing genes
    * Add tests for creation of PrepareGeneCommands

---
+++ src/modules/test/Eryph.Modules.Controller.Tests/Compute/UpdateCatletSagaTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eryph.ConfigModel.Catlets;
+using Eryph.GenePool.Model;
+using Eryph.Modules.Controller.Compute;
+using LanguageExt.Common;
+
+namespace Eryph.Modules.Controller.Tests.Compute;
+
+public class UpdateCatletSagaTests
+{
+    private const string AgentName = "test-agent";
+
+    [Fact]
+    public void CreatePrepareGeneCommands_ValidSources_ReturnsCommands()
+    {
+        var config = new CatletConfig()
+        {
+            Drives =
+            [
+                new CatletDriveConfig()
+                {
+                    Source = "gene:dbosoft/test/1.0:sda"
+                },
+            ],
+            Fodder =
+            [
+                new FodderConfig()
+                {
+                    Source = "gene:dbosoft/test/1.0:test-fodder"
+                },
+            ]
+        };
+
+        var result = UpdateCatletSaga.CreatePrepareGeneCommands(config, AgentName);
+
+        result.Should().BeSuccess().Which.Should().SatisfyRespectively(
+            command =>
+            {
+                command.AgentName.Should().Be(AgentName);
+                command.GeneName.Should().Be("gene:dbosoft/test/1.0:sda");
+                command.GeneType.Should().Be(GeneType.Volume);
+            },
+            command =>
+            {
+                command.AgentName.Should().Be(AgentName);
+                command.GeneName.Should().Be("gene:dbosoft/test/1.0:test-fodder");
+                command.GeneType.Should().Be(GeneType.Fodder);
+            });
+    }
+
+    [Fact]
+    public void CreatePrepareGeneCommands_InformationalParentSource_ReturnsNoCommand()
+    {
+        var config = new CatletConfig()
+        {
+            Fodder =
+            [
+                new FodderConfig()
+                {
+                    Source = "gene:dbosoft/test/1.0:catlet"
+                }
+            ]
+        };
+
+        var result = UpdateCatletSaga.CreatePrepareGeneCommands(config, AgentName);
+
+        result.Should().BeSuccess().Which.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void CreatePrepareGeneCommands_InvalidSources_ReturnsFail()
+    {
+        var config = new CatletConfig()
+        {
+            Drives =
+            [
+                new CatletDriveConfig()
+                {
+                    Source = "gene:invalid|geneset:invalid|drive|gene"
+                },
+            ],
+            Fodder =
+            [
+                new FodderConfig()
+                {
+                    Source = "gene:invalid|geneset:invalid|fodder|gene"
+                },
+            ]
+        };
+
+        var result = UpdateCatletSaga.CreatePrepareGeneCommands(config, AgentName);
+
+        result.Should().BeFail().Which.Should().SatisfyRespectively(
+            error =>
+            {
+                error.Message.Should().Be("The gene source 'gene:invalid|geneset:invalid|drive|gene' is invalid.");
+                error.Inner.Should().BeSome()
+                    .Which.Should().BeOfType<Expected>()
+                    .Which.Message.Should().Be(
+                        "The gene set identifier is malformed. It must be either org/geneset or org/geneset/tag.");
+            },
+            error =>
+            {
+                error.Message.Should().Be("The gene source 'gene:invalid|geneset:invalid|fodder|gene' is invalid.");
+                error.Inner.Should().BeSome()
+                    .Which.Should().BeOfType<Expected>()
+                    .Which.Message.Should().Be(
+                        "The gene set identifier is malformed. It must be either org/geneset or org/geneset/tag.");
+            });
+    }
+}
