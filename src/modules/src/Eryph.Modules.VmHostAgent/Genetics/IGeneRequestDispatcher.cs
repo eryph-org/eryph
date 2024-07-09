b2b1a294068a445bf4ffbc3d4c0f5756b591d60f
Christopher Mann <christopher.mann@dbosoft.eu>
2024-07-09 16:09:03 +0200

    Fix

--- src/modules/src/Eryph.Modules.VmHostAgent/Genetics/IGeneRequestDispatcher.cs
+++ src/modules/src/Eryph.Modules.VmHostAgent/Genetics/IGeneRequestDispatcher.cs
@@ -7,6 +7,4 @@ namespace Eryph.Modules.VmHostAgent.Genetics;
 public interface IGeneRequestDispatcher
 {
     ValueTask NewGeneRequestTask(IOperationTaskMessage message, GeneType geneType, string geneName);
-    ValueTask NewGenomeRequestTask(IOperationTaskMessage message, string genesetName);
-
 }
