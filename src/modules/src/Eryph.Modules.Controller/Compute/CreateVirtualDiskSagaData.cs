6a7561a5b42dc4fb705ced2b215ee14c65e90ea9
Christopher Mann <christopher.mann@dbosoft.eu>
2024-08-23 12:46:16 +0200

    Fix saga data

--- src/modules/src/Eryph.Modules.Controller/Compute/CreateVirtualDiskSagaData.cs
+++ src/modules/src/Eryph.Modules.Controller/Compute/CreateVirtualDiskSagaData.cs
@@ -1,5 +1,4 @@
-﻿using Dbosoft.Rebus.Operations.Workflow;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Eryph.Modules.Controller.Compute;
 
-internal class CreateVirtualDiskSagaData : TaskWorkflowSagaData
+internal class CreateVirtualDiskSagaData
 {
     public string AgentName { get; set; }
 
