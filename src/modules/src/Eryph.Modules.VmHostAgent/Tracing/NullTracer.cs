7790caa4e6ad7397d62ec51a1c911afc6f045034
Christopher Mann <christopher.mann@dbosoft.eu>
2024-09-10 18:11:16 +0200

    * Make tracing configurable
    * Disable tracing by default

---
+++ src/modules/src/Eryph.Modules.VmHostAgent/Tracing/NullTracer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eryph.VmManagement.Tracing;
+
+namespace Eryph.Modules.VmHostAgent.Tracing;
+
+/// <summary>
+/// This implementation of <see cref="ITracer"/> does nothing and
+/// is used when the tracing is disabled.
+/// </summary>
+internal class NullTracer : ITracer
+{
+    public void CloseTrace(Guid traceContext)
+    {
+    }
+
+    public void Write(Guid contextId, string correlationId, TraceData data, string message = null)
+    {
+    }
+}
---
+++ src/modules/src/Eryph.Modules.VmHostAgent/Tracing/TracingConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eryph.Modules.VmHostAgent.Tracing;
+
+internal class TracingConfig
+{
+    public bool Enabled { get; set; }
+}
