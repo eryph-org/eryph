58c45ead7d6d4fb01665fce5ea2dc697db16c63e
Christopher Mann <christopher.mann@dbosoft.eu>
2024-09-23 19:45:15 +0200

    Bump OVN packages

--- src/apps/src/Eryph-zero/Program.cs
+++ src/apps/src/Eryph-zero/Program.cs
@@ -748,8 +748,8 @@ internal static class Program
                     {
                         var ovsEnv = new EryphOVSEnvironment(new EryphOvsPathProvider(ovsPath), loggerFactory);
                         var ovsControl = new OVSControl(ovsEnv);
-                        await using var ovsDbNode = new OVSDbNode(ovsEnv, loggerFactory);
-                        await using var ovsVSwitchNode = new OVSSwitchNode(ovsEnv, loggerFactory);
+                        await using var ovsDbNode = new OVSDbNode(ovsEnv, new LocalOVSWithOVNSettings(), loggerFactory);
+                        await using var ovsVSwitchNode = new OVSSwitchNode(ovsEnv, new LocalOVSWithOVNSettings(), loggerFactory);
                         var cancelSource = new CancellationTokenSource(TimeSpan.FromMinutes(1));
                         // ReSharper disable AccessToDisposedClosure
 
