355df4b1325a6cfe25457e5d9d3bf22eb1d2270a
Christopher Mann <christopher.mann@dbosoft.eu>
2025-03-19 19:37:05 +0100

    Refactor handler

--- src/modules/src/Eryph.Modules.VmHostAgent/VirtualMachineStopHandler.cs
+++ src/modules/src/Eryph.Modules.VmHostAgent/VirtualMachineStopHandler.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Dbosoft.Rebus.Operations;
 using Eryph.Messages.Resources.Catlets.Commands;
 using Eryph.Modules.VmHostAgent.Networks;
-using Eryph.Modules.VmHostAgent.Networks.Powershell;
 using Eryph.VmManagement;
 using Eryph.VmManagement.Data.Full;
-using Eryph.VmManagement.Sys;
 using JetBrains.Annotations;
 using LanguageExt;
-using LanguageExt.Common;
-using LanguageExt.Effects.Traits;
-using Rebus.Handlers;
 
 using static LanguageExt.Prelude;
 
@@ -21,65 +15,18 @@ namespace Eryph.Modules.VmHostAgent;
 internal class VirtualMachineStopHandler(
     ITaskMessaging messaging,
     IServiceProvider serviceProvider)
-    :IHandleMessages<OperationTask<StopVMCommand>>
+    : VirtualMachineStateTransitionHandler<StopVMCommand>(messaging, serviceProvider)
 {
-    public async Task Handle(OperationTask<StopVMCommand> message)
-    {
-        var result = await VirtualMachineStopHandler<AgentRuntime>
-            .HandleCommand(message.Command)
-            .Run(AgentRuntime.New(serviceProvider));
-
-        await result.FailOrComplete(messaging, message);
-    }
-}
-
-internal static class VirtualMachineStopHandler<RT>
-    where RT : struct, HasCancel<RT>, HasPowershell<RT>, HasWmi<RT>
-{
-    public static Aff<RT, Unit> HandleCommand(StopVMCommand command) =>
-        from powershell in default(RT).Powershell
-        let getVmCommand = PsCommandBuilder.Create()
-            .AddCommand("Get-VM")
-            .AddParameter("Id", command.VMId)
-        from vmInfos in powershell.GetObjectsAsync<VirtualMachineInfo>(getVmCommand).ToAff()
-        from vmInfo in vmInfos.HeadOrNone()
-            .ToAff(Error.New($"The VM with ID {command.VMId} was not found."))
-        from _ in command.StopProcess
-        // Improve error handling
-            ? StopVm(vmInfo) | StopVmProcess(command.VMId)
-            : StopVm(vmInfo)
-        select unit;
-
-    private static Aff<RT, Unit> StopVm(
+    protected override Aff<AgentRuntime, Unit> HandleCommand(
+        OperationTask<StopVMCommand> message,
         TypedPsObject<VirtualMachineInfo> vmInfo) =>
-        from powershell in default(RT).Powershell
-        let stopVmCommand = PsCommandBuilder.Create()
-            .AddCommand("Stop-VM")
-            .AddParameter("VM", vmInfo.PsObject)
-            .AddParameter("TurnOff")
-        // TODO add a retry
-        from _ in timeout(
-            TimeSpan.FromSeconds(15),
-            from ct in cancelToken<RT>()
-            from _ in powershell.RunAsync(stopVmCommand).ToAff()
-            select unit)
-        select unit;
-
-    private static Aff<RT, Unit> StopVmProcess(Guid vmId) =>
-        // Unfortunately, the Get-VM Cmdlet does not expose the process ID
-        // of the VM's worker process. Hence, we query Hyper-V via WMI.
-        from processId in WmiQueries<RT>.getVmProcessId(vmId)
-        from _ in processId
-            .Map(StopProcess)
-            .SequenceSerial()
-        select unit;
-
-    private static Aff<RT, Unit> StopProcess(uint processId) =>
-        from powershell in default(RT).Powershell
-        let stopProcessCommand = PsCommandBuilder.Create()
-            .AddCommand("Stop-Process")
-            .AddParameter("Id", processId)
-            .AddParameter("Force")
-        from _ in powershell.RunAsync(stopProcessCommand).ToAff()
-        select unit;
+        timeout(
+            TimeSpan.FromMinutes(5),
+            from powershell in default(AgentRuntime).Powershell
+            let command = PsCommandBuilder.Create()
+                .AddCommand("Stop-VM")
+                .AddParameter("VM", vmInfo.PsObject)
+                .AddParameter("TurnOff")
+            from _1 in powershell.RunAsync(command).ToAff()
+            select unit);
 }
