7787566958e75eed193470fa4ed242cd44fdd342
Christopher Mann <christopher.mann@dbosoft.eu>
2025-03-20 15:00:37 +0100

    * Fix powershell pipeline cancellation
    * FIx kill handler

--- src/modules/src/Eryph.Modules.VmHostAgent/VirtualMachineKillHandler.cs
+++ src/modules/src/Eryph.Modules.VmHostAgent/VirtualMachineKillHandler.cs
@@ -8,7 +8,6 @@ using Eryph.VmManagement.Sys;
 using JetBrains.Annotations;
 using LanguageExt;
 using LanguageExt.Common;
-
 using static LanguageExt.Prelude;
 
 namespace Eryph.Modules.VmHostAgent;
@@ -37,16 +36,16 @@ internal class VirtualMachineKillHandler(
         let getCommand = PsCommandBuilder.Create()
             .AddCommand("Get-VM")
             .AddParameter("Id", vmInfo.Value.Id)
-        /*
+        //from reloaded in Reload(vmInfo.Value.Id)
         // TODO This does not work the VM will still be in status running (is there a way to force a refresh?)
         from optionalVmInfo in powershell.GetObjectAsync<VirtualMachineInfo>(getCommand).ToAff()
         from reloadedVmInfo in optionalVmInfo.ToAff(Error.New(
             $"The VM with ID {vmInfo.Value.Id} was not found."))
         let stopCommand = PsCommandBuilder.Create()
             .AddCommand("Stop-VM")
-            .AddParameter("VM", vmInfo.PsObject)
+            .AddParameter("VM", reloadedVmInfo.PsObject)
             .AddParameter("TurnOff")
         from _2 in powershell.RunAsync(stopCommand).ToAff()
-        */
+        // TODO reload again and issue status update event
         select unit;
 }
