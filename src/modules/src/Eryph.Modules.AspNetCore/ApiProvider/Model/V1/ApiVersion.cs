5b57eb657b5ca0d2a8722ad4334c85f20ce62b6b
Christopher Mann <christopher.mann@dbosoft.eu>
2025-04-02 12:48:57 +0200

    Add endpoint with API version information (#330)

---
+++ src/modules/src/Eryph.Modules.AspNetCore/ApiProvider/Model/V1/ApiVersion.cs
@@ -0,0 +1,21 @@
+﻿namespace Eryph.Modules.AspNetCore.ApiProvider.Model.V1;
+
+/// <summary>
+/// Encodes an API version.
+/// </summary>
+public class ApiVersion
+{
+    /// <summary>
+    /// The major version of the API. The major version is incremented when
+    /// breaking changes are introduced to the API. The version part of the
+    /// endpoint URLs will change in this case.
+    /// </summary>
+    public required int Major { get; init; }
+
+    /// <summary>
+    /// The minor version of the API. The minor version is incremented when
+    /// backwards-compatible extensions are made to the API. This allows
+    /// the clients to support different features sets.
+    /// </summary>
+    public required int Minor { get; init; }
+}
---
+++ src/modules/src/Eryph.Modules.AspNetCore/ApiProvider/Model/V1/ApiVersionResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Eryph.Modules.AspNetCore.ApiProvider.Model.V1;
+
+/// <summary>
+/// Contains versioning information for the REST API.
+/// </summary>
+public class ApiVersionResponse
+{
+    /// <summary>
+    /// The latest version of the API supported by this instance.
+    /// </summary>
+    public required ApiVersion LatestVersion { get; init; } = new()
+    {
+        Major = 1,
+        Minor = 1,
+    };
+}
