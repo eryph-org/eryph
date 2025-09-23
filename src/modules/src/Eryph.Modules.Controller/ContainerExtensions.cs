5395766da54998fc3a7bd082680da7f7d407102e
Christopher Mann <christopher.mann@dbosoft.eu>
2025-09-23 12:30:18 +0200

    Improve update and warmup logic (#361)
    * Remove disposal of global semaphores
    * Decrease timeout for rollback of data directory backup
    * Kill warmup process when necessary
    * Use dedicated logic for warmup which only performs system checks and database migrations
    * Improve progress and error information during warmup
    
    Closes #358

---
+++ src/modules/src/Eryph.Modules.Controller/ContainerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Eryph.Modules.Controller.DataServices;
+using Eryph.Modules.Controller.Networks;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eryph.Modules.Controller;
+
+public static class ContainerExtensions
+{
+    public static void AddStateDbDataServices(
+        this Container container)
+    {
+        container.Register<IVirtualMachineDataService, VirtualMachineDataService>(Lifestyle.Scoped);
+        container.Register<IVirtualMachineMetadataService, VirtualMachineMetadataService>(Lifestyle.Scoped);
+        container.Register<IVMHostMachineDataService, VMHostMachineDataService>(Lifestyle.Scoped);
+
+        container.Register<ICatletIpManager, CatletIpManager>(Lifestyle.Scoped);
+        container.Register<IProviderIpManager, ProviderIpManager>(Lifestyle.Scoped);
+        container.Register<IIpPoolManager, IpPoolManager>(Lifestyle.Scoped);
+        container.Register<INetworkConfigValidator, NetworkConfigValidator>(Lifestyle.Scoped);
+        container.Register<INetworkConfigRealizer, NetworkConfigRealizer>(Lifestyle.Scoped);
+        container.Register<IDefaultNetworkConfigRealizer, DefaultNetworkConfigRealizer>(Lifestyle.Scoped);
+        container.Register<INetworkProvidersConfigRealizer, NetworkProvidersConfigRealizer>(Lifestyle.Scoped);
+    }
+}
