149aa44889e70e6de34a4c53afdb4a7a17d4045e
Christopher Mann <christopher.mann@dbosoft.eu>
2025-10-07 15:43:46 +0200

    Add DB models

---
+++ src/data/src/Eryph.StateDb/Model/CatletSpecification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eryph.StateDb.Model;
+
+public class CatletSpecification : Resource
+{
+    public Guid LatestId { get; set; }
+
+    public required CatletSpecificationVersion Latest { get; set; }
+}
