766fca6d466bea7ce517020248951cf4df2b48fb
Christopher Mann <christopher.mann@dbosoft.eu>
2025-10-17 12:13:09 +0200

    Continue API

--- src/modules/src/Eryph.Modules.ComputeApi/Endpoints/V1/CatletSpecifications/ListVersions.cs
+++ src/modules/src/Eryph.Modules.ComputeApi/Endpoints/V1/CatletSpecifications/ListVersions.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using Eryph.Modules.AspNetCore.ApiProvider;
+using Eryph.Modules.AspNetCore.ApiProvider.Endpoints;
+using Eryph.Modules.AspNetCore.ApiProvider.Handlers;
+using Eryph.Modules.AspNetCore.ApiProvider.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Eryph.Modules.ComputeApi.Model.V1;
 
 namespace Eryph.Modules.ComputeApi.Endpoints.V1.CatletSpecifications;
 
-internal class ListVersions
+public class ListVersions(
+    IListFilteredByProjectRequestHandler<ListFilteredByProjectRequest, CatletSpecificationVersion, StateDb.Model.CatletSpecificationVersion> listRequestHandler,
+    IListEntitySpecBuilder<ListFilteredByProjectRequest, StateDb.Model.CatletSpecificationVersion> specBuilder)
+    : ListEntitiesEndpoint<ListFilteredByProjectRequest, CatletSpecificationVersion, StateDb.Model.CatletSpecificationVersion>(listRequestHandler, specBuilder)
 {
+    [Authorize(Policy = "compute:catlets:read")]
+    [HttpGet("catlet_specifications/{specification_id}/versions")]
+    [SwaggerOperation(
+        Summary = "List all catlet specification versions",
+        Description = "List all catlet specification versions",
+        OperationId = "CatletSpecificationVersions_List",
+        Tags = ["Catlet Specifications"])
+    ]
+    [SwaggerResponse(
+        statusCode: StatusCodes.Status200OK,
+        description: "Success",
+        type: typeof(ListResponse<CatletSpecificationVersion>),
+        contentTypes: ["application/json"])
+    ]
+    public override Task<ActionResult<ListResponse<CatletSpecificationVersion>>> HandleAsync(
+        [FromRoute] ListFilteredByProjectRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        return base.HandleAsync(request, cancellationToken);
+    }
 }
---
+++ src/modules/src/Eryph.Modules.ComputeApi/Endpoints/V1/CatletSpecifications/UpdateCatletSpecificationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+using Eryph.Modules.AspNetCore.ApiProvider.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eryph.Modules.ComputeApi.Endpoints.V1.CatletSpecifications;
+
+public class UpdateCatletSpecificationRequest : SingleEntityRequest
+{
+    [FromBody] public required UpdateCatletSpecificationRequestBody Body { get; set; }
+}
+
+public class UpdateCatletSpecificationRequestBody
+{
+    public Guid? CorrelationId { get; set; }
+
+    public required string Configuration { get; set; }
+}
