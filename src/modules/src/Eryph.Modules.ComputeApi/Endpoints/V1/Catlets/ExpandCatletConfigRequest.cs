291482abb801b68e0c89e004191623e4c363fc07
Frank Wagner <frank2568@gmail.com>
2026-05-12 23:06:35 +0200

    Compatibility changes for new metadata add expand config shim endpoint for old clients

---
+++ src/modules/src/Eryph.Modules.ComputeApi/Endpoints/V1/Catlets/ExpandCatletConfigRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+using Eryph.Modules.AspNetCore.ApiProvider.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eryph.Modules.ComputeApi.Endpoints.V1.Catlets;
+
+public class ExpandCatletConfigRequest : SingleEntityRequest
+{
+    [FromBody] public required ExpandCatletConfigRequestBody Body { get; set; }
+}
+
+public class ExpandCatletConfigRequestBody
+{
+    public Guid? CorrelationId { get; set; }
+
+    public required JsonElement Configuration { get; set; }
+
+    public bool? ShowSecrets { get; set; }
+}
